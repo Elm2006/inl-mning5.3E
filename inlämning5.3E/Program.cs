0f12da1030c5d07ec73904850cc387845f93d503
Elm2006 <isel06002@utb.vaxjo.se>
2023-11-22 10:01:49 +0100

    Add project files.

---
+++ inlämning5.3E/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace inlämning5.__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3];
+            int sum = 0;
+            for (int i = 0;i<3;i++) 
+            {
+                Console.WriteLine("Skriv ett tal");
+                tal[i]=int.Parse(Console.ReadLine());
+                sum += tal[i];
+            }
+            Console.WriteLine("Summan av alla talen är " + sum);
+        }
+    }
+}
